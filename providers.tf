78718d852a9abf6d4fcdbbecac27bce1794c2a22
husam4428 <husam_4428@limu.edu.ly>
2026-06-14 01:43:22 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = ">= 3.75.0" # هنا جبرناه يجيب إصدار حديث يدعم الميزة دي غصباً عنه
     }
   }
 }
