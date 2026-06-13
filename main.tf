4c2f3c0b0491ed61097f649d63c7d5e4cf2e610d
husam4428 <husam_4428@limu.edu.ly>
2026-06-14 01:25:47 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,21 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-
-  tags = {
-    Project     = "Project2"
-    Environment = "production"
-    StudentName = "Husam and Abdelmoez"
-  }
+# 1. قراءة الـ Resource Group الجاهز اللي درته في سويسرا
+data "azurerm_resource_group" "rg" {
+  name = "husam-abdelmoez-proj2-aci-rg"
 }
 
+# 2. بناء الـ Container Group داخل الـ Resource Group المختار مباشرة
 resource "azurerm_container_group" "aci" {
   name                = "husam-abdelmoez-aci"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku                 = "Standard"
   ip_address_type     = "Public"
   dns_name_label      = "cloudscale-husam-app"
-  os_type             = "Linux"
 
   container {
     name   = "webserver"
-    image  = var.container_image
+    image  = "husam4428/cloudscale-app:v1"
     cpu    = "0.5"
     memory = "1.5"
 
@@ -30,8 +26,8 @@ resource "azurerm_container_group" "aci" {
   }
 
   tags = {
-    Project     = "Project2"
     Environment = "production"
+    Project     = "Project2"
     StudentName = "Husam and Abdelmoez"
   }
 }
