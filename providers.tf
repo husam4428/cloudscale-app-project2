d2dee8ce0fad4c8c95af2b3fa0f080aa47458305
husam4428 <husam_4428@limu.edu.ly>
2026-06-14 01:49:15 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,12 +2,16 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.75.0" # هنا جبرناه يجيب إصدار حديث يدعم الميزة دي غصباً عنه
+      version = "~> 3.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {}
-  resource_provider_registrations = "none"
+  # حذفنا السطر المسبب للإيرور، واستبدلناه بقفل الميزات التي تسبب لك الـ 403
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
