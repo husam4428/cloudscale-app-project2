630054643db963d0a05652f9761dea0197c59c4f
husam4428 <husam_4428@limu.edu.ly>
2026-06-14 15:18:11 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,14 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
+variable "resource_group_name" {
+  type    = string
+  default = "husam-abdelmoez-proj2-aci-rg"
 }
 
-provider "azurerm" {
-  features {}
+variable "container_group_name" {
+  type    = string
+  default = "husam-abdelmoez-aci"
+}
+
+variable "image_name" {
+  type    = string
+  default = "husam4428/cloudscale-app:v1"
 }
