62555ef60bc830be392ebb9db1476057f7e7ba9a
husam4428 <husam_4428@limu.edu.ly>
2026-06-14 02:58:02 +0200

    Update Azure Container Group configuration

--- main.tf
+++ main.tf
@@ -1,14 +1,10 @@
-data "azurerm_resource_group" "rg" {
-  name = "husam-abdelmoez-proj2-aci-rg"
-}
-
 resource "azurerm_container_group" "aci" {
   name                = "husam-abdelmoez-aci"
   
-  # 👇 يقرأ المنطقة المفتوحة والمربوطة بالمجلد تلقائياً
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  # 👇 استخدام ريجن الطلاب القياسي لتخطي سياسة الحظر
+  location            = "centralus"
   
+  resource_group_name = "husam-abdelmoez-proj2-aci-rg"
   os_type             = "Linux"
   sku                 = "Standard"
   ip_address_type     = "Public"
@@ -17,8 +13,8 @@ resource "azurerm_container_group" "aci" {
   container {
     name   = "webserver"
     image  = "husam4428/cloudscale-app:v1"
-    cpu    = "0.5"
-    memory = "1.5"
+    cpu    = "1.0"
+    memory = "1.0"
 
     ports {
       port     = 80
