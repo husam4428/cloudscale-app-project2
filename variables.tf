a3f1bac1851f0da97b14e3980e6ec8c895d050b5
husam4428 <husam_4428@limu.edu.ly>
2026-06-14 15:01:32 +0200

    Add Azure client and subscription variables

--- variables.tf
+++ variables.tf
@@ -1,14 +1,19 @@
-variable "resource_group_name" {
-  type    = string
-  default = "husam-abdelmoez-proj2-aci-rg"
+variable "azure_client_id" {
+  type      = string
+  sensitive = true
 }
 
-variable "container_group_name" {
-  type    = string
-  default = "husam-abdelmoez-aci"
+variable "azure_client_secret" {
+  type      = string
+  sensitive = true
 }
 
-variable "image_name" {
-  type    = string
-  default = "husam4428/cloudscale-app:v1"
+variable "azure_subscription_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "azure_tenant_id" {
+  type      = string
+  sensitive = true
 }
