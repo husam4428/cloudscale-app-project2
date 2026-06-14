9790e0b7c3ce8d355d5239c8ebbb9be2cbebeafa
husam4428 <husam_4428@limu.edu.ly>
2026-06-14 14:25:10 +0200

    Remove Azure provider configuration from providers.tf
    Removed Azure provider configuration and related variables.

--- providers.tf
+++ providers.tf
@@ -7,12 +7,6 @@ terraform {
   }
 }
 
-# 👇 ربط الصلاحيات مباشرة داخل الـ Provider لتفادي خطأ الـ CLI
 provider "azurerm" {
   features {}
-
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-  subscription_id = var.azure_subscription_id
-  tenant_id       = var.azure_tenant_id
 }
