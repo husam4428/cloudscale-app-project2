ed4a4ab38bd4379ffc39c4e2db52bdde27515281
husam4428 <husam_4428@limu.edu.ly>
2026-06-14 13:40:47 +0200

    Update outputs.tf to use variables and add new outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
 output "resource_group_name" {
-  value = "husam-abdelmoez-proj2-aci-rg"
+  value = var.resource_group_name
 }
 
-output "vm_public_ip" {
-  value = azurerm_public_ip.publicip.ip_address
+output "aci_public_ip" {
+  value = azurerm_container_group.aci.ip_address
+}
+
+output "aci_fqdn" {
+  value = azurerm_container_group.aci.fqdn
 }
