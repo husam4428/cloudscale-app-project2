0eb5c5be532dc2cb00ee240418be559e0313ab69
husam4428 <husam_4428@limu.edu.ly>
2026-06-14 15:09:06 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,10 +6,7 @@ terraform {
     }
   }
 }
+
 provider "azurerm" {
   features {}
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-  subscription_id = var.azure_subscription_id
-  tenant_id       = var.azure_tenant_id
 }
