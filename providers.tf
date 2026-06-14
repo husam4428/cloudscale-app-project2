d02d1c782441cfe2814c382eb0066bc5816baa12
husam4428 <husam_4428@limu.edu.ly>
2026-06-14 15:17:04 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,10 +9,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  # 👇 حط القيم الصريحة والارقام متاعك هنا مباشرة بين علامات التنصيص
-  client_id       = "اكتب_هنا_الـ_Client_ID"
-  client_secret   = "اكتب_هنا_الـ_Client_Secret"
-  subscription_id = "اكتب_هنا_الـ_Subscription_ID"
-  tenant_id       = "اكتب_هنا_الـ_Tenant_ID"
 }
